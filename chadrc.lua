e732ba351a4cb2b70cf8b87e320c456d02759b4e
jakublaba <jakub.maciej.laba@gmail.com>
2023-05-21 18:18:01 +0200

    Override some nvdash icons missing from JetBrainsMonoNF font

--- chadrc.lua
+++ chadrc.lua
@@ -4,6 +4,14 @@ M.ui = {
   theme = 'tokyodark',
   nvdash = {
     load_on_startup = true,
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc b m", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    }
   }
 }
 M.plugins = "custom.plugins"
